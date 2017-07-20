454357eb6b5a567505867e548737fa2804439be4
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-07-20 17:30:40 -0400

    Added game logic in javascript to determine if a winner exists and alerts the user if such a winner exists

--- game.js
+++ game.js
@@ -13,6 +13,10 @@ var square9 = $('#square9');
 var squares = [null, square1, square2, square3, square4, square5
 , square6, square7, square8, square9];
 
+var winningCombinations = [["1","2","3"],["4","5","6"],["7","8","9"], 
+                            ["1","4","7"],["2","5","8"],["3","6","9"],
+                            ["1","5","9"],["3","5","7"]];
+
 var state = {
     playerOne: "X",
     playerTwo: "O",
@@ -35,11 +39,34 @@ square.click(function() {
         var id = $(this).find('p').attr('id')[6];
         console.log(state.currentTurn);
         state.board[id] = state.currentTurn;
-        state.currentTurn = (state.currentTurn == state.playerOne)?state.playerTwo:state.playerOne;
+        // state.currentTurn = (state.currentTurn == state.playerOne)?state.playerTwo:state.playerOne;
         state.status = (state.status === "Player One's Turn")?"Player Two's Turn":"Player One's Turn";
         render();
+        var didWin = checkWinner();
+        state.currentTurn = (state.currentTurn == state.playerOne)?state.playerTwo:state.playerOne;
+        if(didWin) {
+            resetBoard();
+        }
     }
 });
+
+var resetBoard = function() {
+    
+}
+
+var checkWinner = function() {
+    for(var i = 0; i < winningCombinations.length; i++) {
+        var a = Number(winningCombinations[i][0]);
+        var b = Number(winningCombinations[i][1]);
+        var c = Number(winningCombinations[i][2]);
+        if(state.board[a] === "X" || state.board[b] === "O"){
+            if(state.board[a] === state.board[b] && state.board[b] === state.board[c]) {
+                alert("Winner :)! Congrats " + state.currentTurn + "\nThe game will now reset!");
+                return 5;
+            }
+        }
+    }
+}
 
 
 
